44aedbef6eb6fb99ff7586b58cea890d693fb36f
jebitok-dev <sharonjebitok@gmail.com>
2021-07-28 19:05:00 +0300

    Add House Controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,37 @@
 class HousesController < ApplicationController
+    before_action :set_house, only: [:show, :update, :destroy]
+
+    def index 
+        @houses = House.all
+        json_response(@houses)
+    end
+
+    def create
+        @house = House.create!(house_params)
+        json_response(@house, :created)
+    end
+
+    def show 
+        json_response(@house)
+    end
+
+    def update
+        @house.update(house_params)
+        head :no_content
+    end
+
+    def destroy
+        @house.destroy
+        head :no_content
+    end
+
+    private
+
+    def house_params
+        params.permit(:title, :created_by)
+    end
+
+    def set_house
+        @house = House.find(params[:id])
+    end
 end
