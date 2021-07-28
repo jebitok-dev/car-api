6001f89cf478b5034378f470cde3cc7f4ec874a8
jebitok-dev <sharonjebitok@gmail.com>
2021-07-28 23:38:43 +0300

    Add Favorite Controllers

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,41 @@
 class FavoritesController < ApplicationController
+    before_action :set_house
+    before_action :set_house_favorite, only: [:show, :update, :destroy]
+
+    def index
+        json_response(@house.favorites)
+    end
+
+    def show
+        json_response(@favorites)
+    end
+
+    def create
+        @house.favorites.create!(favorite_params)
+        json_response(@house, :created)
+    end
+
+    def update
+        @favorite.update(favorite_params)
+        head :no_content
+    end
+
+    def destroy
+        @favorite.destroy
+        head :no_content
+    end
+
+    private
+
+    def favorite_params
+        params.permit(:name, :done)
+    end
+
+    def set_house
+        @house = House.find(params[:house_id])
+    end
+
+    def set_house_favorite
+        @favorite = @house.favorites.find_by!(id: params[:id]) if @house
+    end
 end
--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -35,12 +35,87 @@ RSpec.describe 'Favorite API' do
     describe 'GET /houses/:house_id/favorites/:id' do
         before{get "/houses/#{house_id}/favorites/#{id}"}
 
-        context 'when todo item exists' do
+        context 'when house favorite exists' do
             it 'returns status code 200' do
                 expect(response).to have_http_status(200)
             end
 
+            it 'returns a the favorite' do
+                expect(json['id'].to eq(id))
+            end
+        end
+
+        context 'when house favorite does not exist' do 
+            let(:id) {0}
+
+            it 'returns status code 404' do
+                expect(response).to have_http_status(404)
+            end
+
+            it 'returns a not found message' do
+                expect(response.body).to match(/Couldn't find favorite/)
+            end
         end
     end
 
+    describe 'POST /houses/:todo_id/favorites' do 
+        let(:valid_attributes) {{name: 'Apartments', done: false}}
+
+        context 'when request attributes are valid' do 
+            before {post "/houses/#{house_id}/favorites", params: valid_attributes}
+
+            it 'returns status code 201' do
+                expect(response).to have_http_status(201)
+            end
+        end
+
+        context 'when an invalid request' do
+            before {post "/houses/#{house_id}/favorites", params: {}}
+
+            it 'returns status code 422' do
+                expect(response).to have_http_status(422)
+            end
+
+            it 'returns a failure message' do 
+                expect(response.body).to match(/Validation failed: Name can't be blank/)
+            end 
+        end
+    end
+
+    describe 'PUT /houses/:house_id/favorites/:id' do
+        let(:valid_attributes) {{name: 'Flats'}}
+
+        before {put "/houses/#{house_id}/favorites/#{id}", params: valid_attributes}
+
+        context 'when favorite exists' do
+            it 'returns status code 204' do
+                expect(response).to have_http_status(204)
+            end
+
+            it 'updates the favorite' do
+                updated_favorite = Favorite.find(id)
+                expect(updated_favorite.name).to match(/Flats/)
+            end
+        end
+
+        context 'when the item does not exist' do 
+            let(:id) {0}
+
+            it 'returns status code 404' do
+                expect(response).to have_http_status(404)
+            end
+
+            it 'returns a not found' do 
+                expect(response.body).to match(/Couldn't find Favorite/)
+            end
+        end
+    end
+
+    describe 'DELETE /houses/:id' do
+        before {delete "/houses/#{house_id}/favorites/#{id}"}
+
+        it 'returns status code 204' do
+            expect(response).to have_http_status(204)
+        end
+    end
 end
