e00a9d6b84af5f279db2aab56e5e77dd1c8a4949
jebitok-dev <sharonjebitok@gmail.com>
2021-08-02 16:47:08 +0300

    Add Changes to DB

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,5 @@
 class AppointmentsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   include AppointmentsHelper
 
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_08_02_084101) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appointments", force: :cascade do |t|
     t.string "username"
     t.string "model"
@@ -31,22 +34,6 @@ ActiveRecord::Schema.define(version: 2021_08_02_084101) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "favorites", force: :cascade do |t|
-    t.string "name"
-    t.boolean "done"
-    t.integer "house_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["house_id"], name: "index_favorites_on_house_id"
-  end
-
-  create_table "houses", force: :cascade do |t|
-    t.string "title"
-    t.string "created_by"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -55,5 +42,4 @@ ActiveRecord::Schema.define(version: 2021_08_02_084101) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "favorites", "houses"
 end
