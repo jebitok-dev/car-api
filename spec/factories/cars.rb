d4d4dc403084469419a38ba83120a5a2138d9328
jebitok-dev <sharonjebitok@gmail.com>
2021-08-06 15:56:09 +0300

    Add Factory Specs

---
+++ spec/factories/cars.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+    factory :house do
+      price { '36000' }
+      model { 'Models Here' }
+      reviews { 'Reviews about the car' }
+      image { 'Image of the car' }
+      name { 'Name Here' }
+    end
+  end
+  
---
+++ spec/factories/favorites.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+    factory :favorite do
+      user
+      house
+    end
+  end
