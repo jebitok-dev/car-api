e5e670b4bc35aae986eef31b58bf3d6f77a51937
jebitok-dev <sharonjebitok@gmail.com>
2021-08-06 12:14:31 +0300

    Add Changes to Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_140910) do
+ActiveRecord::Schema.define(version: 2021_08_06_090758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,14 @@ ActiveRecord::Schema.define(version: 2021_08_02_140910) do
     t.string "reviews"
     t.string "price"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.integer "car_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
