32510179c322e01ddabed7bb12e3533d7ed5e6fa
jebitok-dev <sharonjebitok@gmail.com>
2021-08-06 12:26:58 +0300

    Add Changes to App Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,40 +1,13 @@
 class ApplicationController < ActionController::API
-  # skip_before_action :verify_authenticity_token
-  before_action :authorized
+include Response
+include ExceptionHandler
 
-  def encode_token(payload)
-    JWT.encode(payload, 's3cr3t')
-  end
+before_action :authorize_user
+attr_reader :current_user
 
-  def auth_header
-    # { Authorization: 'Bearer <token>' }
-    request.headers['Authorization']
-  end
+private
 
-  def decoded_token
-    return unless auth_header
-
-    token = auth_header.split[1]
-    # header: { 'Authorization': 'Bearer <token>' }
-    begin
-      JWT.decode(token, 's3cr3t', true, algorithm: 'HS256')
-    rescue JWT::DecodeError
-      nil
-    end
-  end
-
-  def logged_in_user
-    return unless decoded_token
-
-    user_id = decoded_token[0]['user_id']
-    @user = User.find_by(id: user_id)
-  end
-
-  def logged_in?
-    logged_in_user
-  end
-
-  def authorized
-    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  def authorize_user
+    @current_user =(AuthorizeApiRequest.new(request.headers).call)[:user]
   end
 end
